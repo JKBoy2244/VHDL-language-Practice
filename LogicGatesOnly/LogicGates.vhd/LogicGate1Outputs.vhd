fa1f7ff4244d5f8f2752152fcfc36991a79dd42e
JKBoy2244 <jobayer2006@gmail.com>
2026-02-15 23:42:07 +0000

    Create LogicGate1Outputs.vhd

---
+++ LogicGatesOnly/LogicGates.vhd/LogicGate1Outputs.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity logic_diagram is 
+
+  port (
+
+    A, B, C_i : in std_logic;  --These are all the 3 inputs from the respective diagram
+    C_o, S : out std_logic     --These are all the 2 outputs from the respective diagram
+
+  );
+
+end logic_diagram;
+
+architecture behavioural of logic_diagram is 
+
+begin
+
+  C_o <= '1' when (A = '0' and B = '1' and C = '1') else
+         '1' when (A = '1' and B = '0' and C = '1') else    --Conditions for C_o to be 1
+         '1' when (A = '1' and B = '1' and C = '0') else
+         '1' when (A = '1' and B = '1' and C = '1') else
+         '0';   --Default if anything else
+
+ S <= '1' when (A = '0' and B = '0' and C = '1') else
+      '1' when (A = '0' and B = '1' and C = '0') else      --Conditions for C_o to be 1
+      '1' when (A = '1' and B = '0' and C = '0') else
+      '1' when (A = '1' and B = '1' and C = '1') else
+      '0';    --Default if anything else
+  
+end behavioural;
