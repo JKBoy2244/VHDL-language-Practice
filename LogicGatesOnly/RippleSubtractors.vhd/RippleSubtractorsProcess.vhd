caebd978c471131c51869515b4b49cd52de5f65c
JKBoy2244 <jobayer2006@gmail.com>
2026-03-22 22:17:43 +0000

    Create RippleSubtractorsProcess.vhd

---
+++ LogicGatesOnly/RippleSubtractors.vhd/RippleSubtractorsProcess.vhd
@@ -0,0 +1,46 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity ripple_subtractor_proc is 
+
+  port (
+
+    A_0 , A_1 , A_2 , A_3 : in std_logic;    --All 4 (A) inputs for the 4-bit ripple carry adder.
+    B_0 , B_1 , B_2 , B_3 : in std_logic;    --All 4 (B) inputs for the 4-bit ripple carry adder.
+    C_in : in std_logic;                     --Carry input for the 4-bit ripple carry adder.
+    S_0 , S_1 , S_2 , S_3 : out std_logic;   --All 4 (S) outputs for the 4-bit ripple carry adder.
+    C_3 : out std_logic                      --Carry output for the 4-bit ripple carry adder.
+
+  );
+
+end ripple_subtractor_proc;
+
+architecture behavioural of ripple_subtractor_proc is
+begin
+
+  ripple_subtractor_process: process(A_0 , A_1 , A_2 , A_3 , B_0 , B_1 , B_2 , B_3 , C_in)
+    variable C_0, C_1, C_2: std_logic;
+
+  begin
+
+    S_0 <= '0';
+    S_1 <= '0';
+    S_2 <= '0';    --Outputs initialised
+    S_3 <= '0';
+    C_3 <= '0';
+
+    S_0 <= ((A_0 xor (not B_0)) xor C_in);                            --Individual equations for S_0 and C_0 respectively
+    C_0 := (A_0 and (not B_0)) or ((A_0 xor (not B_0)) and C_in);
+
+    S_1 <= ((A_1 xor (not B_1)) xor C_0);                             --Individual equations for S_0 and C_0 respectively
+    C_1 := (A_1 and (not B_1)) or ((A_1 xor (not B_1)) and C_0);
+
+    S_2 <= ((A_2 xor (not B_2)) xor C_1);                            --Individual equations for S_0 and C_0 respectively    (:= is used as some variable components are signals (both inputs and outputs))
+    C_2 := (A_2 and (not B_2)) or ((A_2 xor (not B_2)) and C_1);
+
+    S_3 <= ((A_3 xor (not B_3)) xor C_2);                            --Individual equations for S_0 and C_0 respectively
+    C_3 <= (A_3 and (not B_3)) or ((A_3 xor (not B_3)) and C_2);
+
+ end process ripple_subtractor_process;
+end behavioural;
+    
