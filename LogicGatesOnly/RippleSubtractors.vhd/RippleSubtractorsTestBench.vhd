29b4e46d52914588d11bcda3be38774f8bada689
JKBoy2244 <jobayer2006@gmail.com>
2026-03-23 22:27:24 +0000

    Create RippleSubtractorsTestBench.vhd

---
+++ LogicGatesOnly/RippleSubtractors.vhd/RippleSubtractorsTestBench.vhd
@@ -0,0 +1,36 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity rippleSubtractors_testbench is
+end rippleSubtractors_testbench;
+
+architecture behavioural of rippleSubtractors_testbench is
+  component rippleSubtractors_test is
+
+   port (
+
+    A_0 , A_1 , A_2 , A_3 : in std_logic;    --All 4 (A) inputs for the 4-bit ripple carry adder.
+    B_0 , B_1 , B_2 , B_3 : in std_logic;    --All 4 (B) inputs for the 4-bit ripple carry adder.
+    C_in : in std_logic;                     --Carry input for the 4-bit ripple carry adder.
+    S_0 , S_1 , S_2 , S_3 : out std_logic;   --All 4 (S) outputs for the 4-bit ripple carry adder.
+    C_3 : out std_logic                      --Carry output for the 4-bit ripple carry adder.
+
+  );
+
+ end component;
+ signal A_0 , A_1 , A_2 , A_3, B_0 , B_1 , B_2 , B_3, C_in, S_0 , S_1 , S_2 , S_3, C_3: std_logic;
+
+ begin
+
+   uut: rippleSubtractors_test port map(A_0 => A_0, A_1 => A_1,  A_2 => A_2,  A_3 => A_3,  B_0 => B_0,  B_1 => B_1,  B_2 => B_2,  B_3 => B_3,  C_in => C_in,  S_0 => S_0,  S_1 => S_1,  S_2 => S_2,  S_3 => S_3,  C_3 => C_3);
+   rippleSubtractors_process: process
+
+   begin
+     
+     A_0 <= '0'; A_1 <= '0';  A_2 <= '0';  A_3 <= '0';  B_0 <= '0';  B_1 <= '0';  B_2 <= '0';  B_3 <= '0';  C_in <= '1'; wait for 10 ns;
+     A_0 <= '1'; A_1 <= '0'; A_2 <= '0'; A_3 <= '0';  B_0 <= '1'; B_1 <= '0'; B_2 <= '0'; B_3 <= '0';  C_in <= '1'; wait for 10 ns;
+     A_0 <= '1'; A_1 <= '0'; A_2 <= '1'; A_3 <= '0';  B_0 <= '1'; B_1 <= '1'; B_2 <= '0'; B_3 <= '0';  C_in <= '1'; wait for 10 ns;   --Just a few test cases since doing 2^8 combinations each would be too exhaustive
+     
+     wait;
+  end process;
+end behavioural;
